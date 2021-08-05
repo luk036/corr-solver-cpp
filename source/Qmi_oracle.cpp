80993efaa4feb846836897146ceae14b8afc4b73
Wai-Shing Luk <luk036@gmail.com>
2021-08-05 10:23:04 +0800

    replace the ROW macro with xt::row

--- source/Qmi_oracle.cpp
+++ source/Qmi_oracle.cpp
@@ -2,8 +2,8 @@
 #include <corrsolver/Qmi_oracle.hpp>
 #include <xtensor-blas/xlinalg.hpp>
 
-#define ROW(X, index) xt::view(X, index, xt::all())
-#define COLUMN(X, index) xt::view(X, xt::all(), index)
+// #define ROW(X, index) xt::view(X, index, xt::all())
+// #define COLUMN(X, index) xt::view(X, xt::all(), index)
 
 using Arr = xt::xarray<double, xt::layout_type::row_major>;
 using Cut = std::tuple<Arr, double>;
@@ -24,12 +24,12 @@ std::optional<Cut> Qmi_oracle::operator()(const Arr& x) {
         assert(i >= j);
         if (this->_count < i + 1) {
             this->_count = i + 1;
-            ROW(this->_Fx, i) = COLUMN(this->_F0, i);
+            xt::row(this->_Fx, i) = xt::col(this->_F0, i);
             for (auto k = 0U; k != this->_nx; ++k) {
-                ROW(this->_Fx, i) -= COLUMN(this->_F[k], i) * x(k);
+                xt::row(this->_Fx, i) -= xt::col(this->_F[k], i) * x(k);
             }
         }
-        auto a = -dot(ROW(this->_Fx, i), ROW(this->_Fx, j))();
+        auto a = -dot(xt::row(this->_Fx, i), xt::row(this->_Fx, j))();
         if (i == j) {
             a += this->_t;
         }
@@ -42,12 +42,12 @@ std::optional<Cut> Qmi_oracle::operator()(const Arr& x) {
 
     const auto ep = this->_Q.witness();
     const auto [start, stop] = this->_Q.p;
-    const auto v = xt::view(this->_Q.witness_vec, xt::range(start, stop));
-    const auto Fxp = ROW(this->_Fx, xt::range(start, stop));
+    const auto v = xt::view(this->_Q.witness_vec, xt::range(start, stop), xt::all());
+    const auto Fxp = xt::view(this->_Fx, xt::range(start, stop));
     const auto Av = dot(v, Fxp);
     auto g = zeros({this->_nx});
     for (auto k = 0U; k != this->_nx; ++k) {
-        const auto Fkp = ROW(this->_F[k], xt::range(start, stop));
+        const auto Fkp = xt::view(this->_F[k], xt::range(start, stop), xt::all());
         g(k) = -2 * dot(dot(v, Fkp), Av)();
     }
     return {{std::move(g), ep}};
