e123f2e044a0c8f253507605d8b6b1fecbc34418
Wai-Shing Luk <luk036@gmail.com>
2021-11-13 05:04:54 +0000

    reformat using --target fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,16 +1,21 @@
-CPMAddPackage("gh:microsoft/GSL@3.1.0")
+cpmaddpackage("gh:microsoft/GSL@3.1.0")
 
-CPMAddPackage(
-  NAME fmt
-  GIT_TAG 7.1.3
-  GITHUB_REPOSITORY fmtlib/fmt
-  OPTIONS "FMT_INSTALL YES" # create an installable target
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  7.1.3
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
 )
 
 find_package(OpenBLAS QUIET)
 if(OpenBLAS_FOUND)
   message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
-  # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE ${OpenBLAS_INCLUDE_DIRS})
+  # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE
+  # ${OpenBLAS_INCLUDE_DIRS})
   include_directories(${OpenBLAS_INCLUDE_DIRS})
 endif(OpenBLAS_FOUND)
 
@@ -26,36 +31,39 @@ if(BLAS_FOUND)
   include_directories(${BLAS_INCLUDE_DIRS})
 endif(BLAS_FOUND)
 
-CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
+cpmaddpackage("gh:xtensor-stack/xtl#0.6.23")
 if(xtl_ADDED)
   message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
   include_directories(${xtl_SOURCE_DIR}/include)
 endif(xtl_ADDED)
 
-CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
+cpmaddpackage("gh:xtensor-stack/xtensor#0.22.0")
 if(xtensor_ADDED)
   message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
   include_directories(${xtensor_SOURCE_DIR}/include)
 endif(xtensor_ADDED)
 
-CPMAddPackage("gh:xtensor-stack/xtensor-blas#0.18.0")
+cpmaddpackage("gh:xtensor-stack/xtensor-blas#0.18.0")
 if(xtensor-blas_ADDED)
   message(STATUS "Found xtensor-blas: ${xtensor-blas_SOURCE_DIR}")
   include_directories(${xtensor-blas_SOURCE_DIR}/include)
 endif(xtensor-blas_ADDED)
 # remember to turn off the warnings
 
-CPMAddPackage(
-  NAME EllAlgo
-  GIT_TAG 1.0.4
-  GITHUB_REPOSITORY luk036/ellalgo-cpp
-  OPTIONS "INSTALL_ONLY YES" # create an installable target
+cpmaddpackage(
+  NAME
+  EllAlgo
+  GIT_TAG
+  1.0.4
+  GITHUB_REPOSITORY
+  luk036/ellalgo-cpp
+  OPTIONS
+  "INSTALL_ONLY YES" # create an installable target
 )
 
 if(WIN32)
   add_definitions(-DXTENSOR_USE_FLENS_BLAS)
 endif()
 
-set(SPECIFIC_LIBS EllAlgo::EllAlgo ${OpenBLAS_LIBRARIES} ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES}
-                  fmt::fmt GSL
-)
+set(SPECIFIC_LIBS EllAlgo::EllAlgo ${OpenBLAS_LIBRARIES} ${LAPACK_LIBRARIES}
+                  ${BLAS_LIBRARIES} fmt::fmt GSL)
