ac36e6a15b99fc75b82d6d965a0a143b76a4df85
Wai-Shing Luk <luk036@gmail.com>
2023-02-15 15:36:44 +0800

    apply fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,55 +1,42 @@
-cpmaddpackage(
-  NAME
-  fmt
-  GIT_TAG
-  7.1.3
-  GITHUB_REPOSITORY
-  fmtlib/fmt
-  OPTIONS
-  "FMT_INSTALL YES" # create an installable target
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-cpmaddpackage("gh:xtensor-stack/xtl#0.6.23")
+CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
 if(xtl_ADDED)
   message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
   include_directories(${xtl_SOURCE_DIR}/include)
 endif(xtl_ADDED)
 
-cpmaddpackage("gh:xtensor-stack/xtensor#0.22.0")
+CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
 if(xtensor_ADDED)
   message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
   include_directories(${xtensor_SOURCE_DIR}/include)
 endif(xtensor_ADDED)
 
-cpmaddpackage(
-  NAME
-  EllAlgo
-  GIT_TAG
-  1.2
-  GITHUB_REPOSITORY
-  luk036/ellalgo-cpp
-  OPTIONS
-  "INSTALL_ONLY YES" # create an installable target
+CPMAddPackage(
+  NAME EllAlgo
+  GIT_TAG 1.2
+  GITHUB_REPOSITORY luk036/ellalgo-cpp
+  OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
-cpmaddpackage("gh:microsoft/GSL@3.1.0")
+CPMAddPackage("gh:microsoft/GSL@3.1.0")
 
-cpmaddpackage(
-  NAME
-  LmiSolver
-  GIT_TAG
-  1.2
-  GITHUB_REPOSITORY
-  luk036/lmi-solver-cpp
-  OPTIONS
-  "INSTALL_ONLY YES" # create an installable target
+CPMAddPackage(
+  NAME LmiSolver
+  GIT_TAG 1.2
+  GITHUB_REPOSITORY luk036/lmi-solver-cpp
+  OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
 find_package(OpenBLAS QUIET)
 if(OpenBLAS_FOUND)
   message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
-  # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE
-  # ${OpenBLAS_INCLUDE_DIRS})
+  # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE ${OpenBLAS_INCLUDE_DIRS})
   include_directories(${OpenBLAS_INCLUDE_DIRS})
 endif(OpenBLAS_FOUND)
 
@@ -65,7 +52,7 @@ if(BLAS_FOUND)
   include_directories(${BLAS_INCLUDE_DIRS})
 endif(BLAS_FOUND)
 
-cpmaddpackage("gh:xtensor-stack/xtensor-blas#0.18.0")
+CPMAddPackage("gh:xtensor-stack/xtensor-blas#0.18.0")
 if(xtensor-blas_ADDED)
   message(STATUS "Found xtensor-blas: ${xtensor-blas_SOURCE_DIR}")
   include_directories(${xtensor-blas_SOURCE_DIR}/include)
@@ -83,4 +70,5 @@ set(SPECIFIC_LIBS
     ${LAPACK_LIBRARIES}
     ${BLAS_LIBRARIES}
     fmt::fmt
-    GSL)
+    GSL
+)
