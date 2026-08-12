6dc30c62b6737042e23d28694f27a83d9f1a0ac7
Wai-Shing Luk <luk036@gmail.com>
2026-08-12 21:49:57 +0800

    build: add nanobench dependency for benchmark target
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- xmake.lua
+++ xmake.lua
@@ -9,6 +9,7 @@ end
 add_requires("fmt", { alias = "fmt" })
 add_requires("doctest", { alias = "doctest" })
 add_requires("spdlog", { alias = "spdlog" })
+add_requires("nanobench", { alias = "nanobench" })
 
 if is_mode("coverage") then
 	add_cxflags("-ftest-coverage", "-fprofile-arcs", { force = true })
@@ -70,7 +71,7 @@ add_includedirs(ellalgo_inc, { public = true })
 add_includedirs(ldsgen_inc, { public = true })
 add_files("benchmark/source/*.cpp")
 add_files("source/qmi_oracle.cpp")
-add_packages("fmt")
+add_packages("fmt", "nanobench")
 
 target("EllAlgo")
 set_kind("static")
